d8f53bf1405e8c8cdde356f7fd1623672d1bcc24
NevilleAdrian <neville.chukumah@gmail.com>
2023-03-04 01:04:56 +0000

    pushed changes

---
+++ app/app/tests.py
@@ -0,0 +1,24 @@
+"""
+Sample test
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test calc module"""
+
+    def test_add_number(self):
+
+        res = calc.add(5,6)
+    
+        self.assertEqual(res, 11) 
+
+
+    def test_substract_number(self):
+
+        res = calc.subtract(15,10)
+
+        self.assertEqual(res, 5) 
+
+        
